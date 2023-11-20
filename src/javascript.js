d225f47b7612f16d45398a11c7afff3232df35dd
Coral-Gs <113552558+Coral-Gs@users.noreply.github.com>
2023-11-20 23:48:19 +0100

    Readjusted responsive design

---
+++ src/javascript.js
@@ -0,0 +1,6 @@
+/*Función para desplegar menú de navegación*/
+
+function toggleMenu() {
+  var navLinksContainer = document.getElementById("nav-links-container");
+  navLinksContainer.classList.toggle("show");
+}
