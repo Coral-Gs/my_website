8698bb110b0a340002beb2163aa1b059d82e54c1
Coral-Gs <113552558+Coral-Gs@users.noreply.github.com>
2023-11-21 12:19:29 +0100

    Updated responsive

--- src/javascript.js
+++ src/javascript.js
@@ -1,6 +1,1 @@
-/*Función para desplegar menú de navegación*/
 
-function toggleMenu() {
-  var navLinksContainer = document.getElementById("nav-links-container");
-  navLinksContainer.classList.toggle("show");
-}
